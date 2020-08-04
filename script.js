9bdd5512de11f2c32a1fddc4c42755916ddf9d0f
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-08-04 10:49:38 +0600

    done

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function handleProductChange(product, isIncrease) {
     }
 
     document.querySelector("#" + product + "-count").value = productNewCount;
-    
+
     let productTotal = 0;
 
     if (product == "phone") {
@@ -19,6 +19,21 @@ function handleProductChange(product, isIncrease) {
     }
 
     document.querySelector("#" + product + "-total").innerHTML = "$" + productTotal;
+    calculateTotal();
+}
+
+function calculateTotal() {
+    const phoneCount = getInputValue("phone");
+    const caseCount = getInputValue("case");
+
+    const totalPrice = phoneCount * 1219 + caseCount * 59;
+    document.querySelector("#total-price").innerHTML = "$" + totalPrice;
+    
+    const tax = Math.round(totalPrice * 0.15);
+    document.querySelector("#tax-amount").innerHTML = "$" + tax;
+
+    const grandTotal = totalPrice + tax;
+    document.querySelector("#grand-total").innerHTML = "$" + grandTotal;
 }
 
 function getInputValue(product) {
